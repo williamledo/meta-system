83efa0d5e75943ae7d9a4da75b9bb06f360133c1
William Lêdo <williamledoc@gmail.com>
2022-10-09 00:26:20 -0300

    first

---
+++ src/main/java/com/william/meta/services/SaleService.java
@@ -0,0 +1,32 @@
+package com.william.meta.services;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.william.meta.entities.Sale;
+import com.william.meta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
+		
+	}
+	
+}
